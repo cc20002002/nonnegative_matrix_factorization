86b794ffbf32401cebda11bddaa2eb73c5315363
Xiaodan Gan <xiaodangan@vlan-2656-10-16-228-154.student.wireless.sydney.edu.au>
2018-09-09 13:55:35 +1000

    fix indent error

--- nmf/algorithm.py
+++ nmf/algorithm.py
@@ -43,14 +43,14 @@ def multiplication_euclidean(V, r):
         The new representation of image data V
 
     """
-	m=V.shape[0]
-	n=V.shape[1]
-	W=np.random.rand(m,r)
-	H=np.random.rand(r,n)
-    	for i in range(1000):
-        	H=H*(W.T@V)/(W.T@W@H)
-        	W=W*(V@H.T)/(W@H@H.T)
-	return W, H
+    m=V.shape[0]
+    n=V.shape[1]
+    W=np.random.rand(m,r)
+    H=np.random.rand(r,n)
+    for i in range(1000):
+        H=H*(W.T@V)/(W.T@W@H)
+        W=W*(V@H.T)/(W@H@H.T)
+    return W, H
 
 def multiplication_divergence(V, r):
     """Set up a benchmark model using NMF in scikit-learn.
@@ -69,17 +69,17 @@ def multiplication_divergence(V, r):
         The new representation of image data V
 
     """
-	m=V.shape[0]
-	n=V.shape[1]
-	W=np.random.rand(m,r)
-	H=np.random.rand(r,n)
-   	for i in range(1000):
-        	VWH = V/(W @ H)
-	        Numerator1=W.T@VWH
-        	H=H*Numerator1/np.sum(W,0).reshape(r,1)
-        	#only when I looked https://au.mathworks.com/matlabcentral/answers/316708-non-negative-matrix-factorization
-        	#I realised I need to calculate VWH again during the update.. waste me 1 hour..
-        	VWH = V / (W @ H)
-        	Numerator2=VWH@H.T
-        	W = W * Numerator2 / np.sum(H, 1).reshape(1,r)
-	return W, H
+    m=V.shape[0]
+    n=V.shape[1]
+    W=np.random.rand(m,r)
+    H=np.random.rand(r,n)
+    for i in range(1000):
+        VWH = V/(W @ H)
+        Numerator1=W.T@VWH
+        H=H*Numerator1/np.sum(W,0).reshape(r,1)
+    	#only when I looked https://au.mathworks.com/matlabcentral/answers/316708-non-negative-matrix-factorization
+    	#I realised I need to calculate VWH again during the update.. waste me 1 hour..
+        VWH = V / (W @ H)
+        Numerator2=VWH@H.T
+        W = W * Numerator2 / np.sum(H, 1).reshape(1,r)
+    return W, H
