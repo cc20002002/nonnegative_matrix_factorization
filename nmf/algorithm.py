8b0ff88302561c137dd7ea1c1da51ab2d1b0751a
Xiaodan Gan <xiaodangan@192-168-1-2.tpgi.com.au>
2018-09-10 16:38:08 +1000

    add MSE check in iterations

--- nmf/algorithm.py
+++ nmf/algorithm.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.decomposition import NMF
 
 niter = 1000
-
+min_error = 0.00001
 
 def benchmark(V, r):
     """Set up a benchmark model using NMF in scikit-learn.
@@ -55,7 +55,7 @@ def truncated_cauchy(V, r):
     for t in range(niter):
         E = V - W.dot(H)
         Q = 1. / (1 + (E / gamma) ** 2)
-        
+
 
 
 def multiplication_euclidean(V, r):
@@ -79,9 +79,22 @@ def multiplication_euclidean(V, r):
     n=V.shape[1]
     W=np.random.rand(m,r)
     H=np.random.rand(r,n)
-    for i in range(1000):
-        H=H*(W.T@V)/(W.T@W@H)
-        W=W*(V@H.T)/(W@H@H.T)
+
+    for i in range(niter):
+        H_u=H*(W.T@V)/(W.T@W@H)
+        W_u=W*(V@H_u.T)/(W@H_u@H_u.T)
+
+        #calculate the distance between iteration
+        e_W = np.sqrt(np.sum((W_u - W)**2, axis = (0,1)))/W.size
+        e_H = np.sqrt(np.sum((H_u - H)**2, axis = (0,1)))/H.size
+
+        #stop iteration if distance less than min_error
+        if e_W < min_error and e_H < min_error:
+            print("iterated: ", i, "times")
+            break
+        else:
+            H = H_u
+            W = W_u
     return W, H
 
 
@@ -106,13 +119,25 @@ def multiplication_divergence(V, r):
     n=V.shape[1]
     W=np.random.rand(m,r)
     H=np.random.rand(r,n)
-    for i in range(4000):
+
+    for i in range(niter):
+        H_old = H
+        W_old = W
+
         VWH = V/(W @ H)
         Numerator1=W.T@VWH
         H=H*Numerator1/np.sum(W,0).reshape(r,1)
     	#only when I looked https://au.mathworks.com/matlabcentral/answers/316708-non-negative-matrix-factorization
-    	#I realised I need to calculate VWH again during the update.. waste me 1 hour..
         VWH = V / (W @ H)
         Numerator2=VWH@H.T
         W = W * Numerator2 / np.sum(H, 1).reshape(1,r)
+
+        #calculate the distance between iteration
+        e_W = np.sqrt(np.sum((W - W_old)**2, axis = (0,1)))/W.size
+        e_H = np.sqrt(np.sum((H - H_old)**2, axis = (0,1)))/H.size
+
+        #stop iteration if distance less than min_error
+        if e_W < min_error and e_H < min_error:
+            print("iterated: ", i, "times")
+            break
     return W, H
