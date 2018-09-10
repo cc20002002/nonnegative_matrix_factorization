6df15db5061de4b9068411187f72eb4f0edda5f2
chen chen <chen.chen.adl@gmail.com>
2018-09-10 17:11:29 +1000

    parallel programming
    improved the speed by at least 25%, maybe 70% for more heavy computations.

--- nmf/main.py
+++ nmf/main.py
@@ -3,18 +3,21 @@ import os
 import pandas as pd
 from nmf import io, util, metric, algorithm
 import matplotlib.pyplot as pl
-
+#from functools import partial
+import multiprocessing 
+from itertools import repeat
+import time
 # Configuration
 sample_index = 100
 sample_size = 0.9
-epoch = 10
-cmap = pl.cm.gray
+epoch = os.cpu_count()
+cmap = pl.cm.Greys
 random_state = 0
 reduce_scale_yaleB = 4
 reduce_scale_orl = 3
 orl_img_size = (92, 112)
 yaleB_img_size = (168, 192)
-
+parallel_flag=0
 model = {
     #"Benchmark (scikit-learn)": algorithm.benchmark,
     "Multiplication KL Divergence": algorithm.multiplication_divergence,
@@ -24,6 +27,8 @@ model = {
 
 Noise = ["Poisson","Normal"]
 
+ 
+
 def main():
     """Run NMF on CroppedYaleB and ORL dataset."""
     if os.name == 'nt123':
@@ -32,6 +37,47 @@ def main():
     else:
         train("data/ORL")
         # train("data/CroppedYaleB")
+
+def one_simulation(i,Vhat,Yhat,n,size,metrics):
+    print("Epoch {}...".format(i + 1))
+    # sample 90% of samples
+    index = np.random.choice(np.arange(n), size, replace=False)
+    subVhat, subYhat = Vhat[:, index], Yhat[index]
+    for noise_fun in Noise:
+        if noise_fun=='Normal':
+            V_noise = np.random.normal(0, 5, subVhat.shape) #* np.sqrt(subVhat)
+            V=subVhat+V_noise
+            V[V<0]=0
+        elif noise_fun=='Poisson':
+            V = np.random.poisson(subVhat)
+            V_noise = V-subVhat
+        
+            # if i == 0:
+            #     draw_image(V, subVhat, V_noise, sample_index)
+
+        r = np.unique(Yhat).shape[0]
+        # loop through different models
+        for name, algo in model.items():
+            name=name+' '+noise_fun               
+            print(name)
+            W, H = algo(V, r)
+            Ypred = util.assign_cluster_label(H.T, subYhat)
+
+            # evaluate metrics
+            _rre = metric.eval_rre(subVhat, W, H)
+            _acc = metric.eval_acc(subYhat, Ypred)
+            _nmi = metric.eval_nmi(subYhat, Ypred)
+            print("RRE = {}".format(_rre))
+            print("ACC = {}".format(_acc))
+            print("NMI = {}".format(_nmi))
+
+            metrics["rre"][name].append(_rre)
+            metrics["acc"][name].append(_acc)
+            metrics["nmi"][name].append(_nmi)
+    return metrics
+
+    # performace over epochs
+
 
 def train(data_name):
     """Run NMF on data stored in data_name."""
@@ -48,61 +94,52 @@ def train(data_name):
             metrics["rre"][name] = []
             metrics["acc"][name] = []
             metrics["nmi"][name] = []
-    for i in range(epoch):
-        print("Epoch {}...".format(i + 1))
-        # sample 90% of samples
-        index = np.random.choice(np.arange(n), size, replace=False)
-        subVhat, subYhat = Vhat[:, index], Yhat[index]
-        for noise_fun in Noise:
-            if noise_fun=='Normal':
-                V_noise = np.random.normal(0, 5, subVhat.shape) #* np.sqrt(subVhat)
-                V=subVhat+V_noise
-            elif noise_fun=='Poisson':
-                V = np.random.poisson(subVhat)
-                V_noise = V-subVhat
-            V[V<0]=0
-                # if i == 0:
-                #     draw_image(V, subVhat, V_noise, sample_index)
-
-            r = np.unique(Yhat).shape[0]
-            # loop through different models
-            for name, algo in model.items():
-                name=name+' '+noise_fun               
-                print(name)
-                W, H = algo(V, r)
-                Ypred = util.assign_cluster_label(H.T, subYhat)
-
-                # evaluate metrics
-                _rre = metric.eval_rre(subVhat, W, H)
-                _acc = metric.eval_acc(subYhat, Ypred)
-                _nmi = metric.eval_nmi(subYhat, Ypred)
-                print("RRE = {}".format(_rre))
-                print("ACC = {}".format(_acc))
-                print("NMI = {}".format(_nmi))
-
-                metrics["rre"][name].append(_rre)
-                metrics["acc"][name].append(_acc)
-                metrics["nmi"][name].append(_nmi)
-
-    # performace over epochs
+    t = time.time()
+    if parallel_flag:
+        pool = multiprocessing.Pool(os.cpu_count())
+        #sim=partial(one_simulation,Vhat=Vhat,Yhat=Yhat,n=n,size=size,metrics=metrics)
+        
+        #pool.starmap(sim, (range(epoch),))
+        metrics=pool.starmap(one_simulation,zip(range(epoch),repeat(Vhat),repeat(Yhat),repeat(n),repeat(size),repeat(metrics)))
+        pool.close()
+        pool.join()
+    else:
+        result=[]
+        for i in range(epoch):
+            temp=one_simulation(i,Vhat,Yhat,n,size,metrics)
+            result.append(temp)
+        metrics=result          
+    t = time.time()-t   
+    print('done')
     mean_metrics = {}
     for mname in ["rre", "acc", "nmi"]:
         mean_metrics[mname] = {}
         for name in model:
             for noise_fun in Noise:
-                mean_metrics[mname][name+' '+noise_fun] = np.mean(metrics[mname][name+' '+noise_fun])
+                mean_metrics[mname][name+' '+noise_fun]=0
+                for i in range(epoch):
+                    mean_metrics[mname][name+' '+noise_fun] += metrics[i][mname][name+' '+noise_fun][0]
+                mean_metrics[mname][name+' '+noise_fun]=mean_metrics[mname][name+' '+noise_fun]/epoch
     df = pd.DataFrame.from_dict(mean_metrics)
     print(df)
+    import IPython; IPython.embed() 
     for name in model:
         for noise_fun in Noise:
             rres = metrics["rre"][name+' '+noise_fun]
             pl.plot(range(epoch), np.log(rres), label=name+' '+noise_fun)
-    pl.legend(loc="bottom right")
+    pl.legend(loc="lower right")
     pl.xlabel("epoch")
     pl.ylabel("relative reconstruction error")
     pl.title("Model comparison of RRE")
     pl.show()
-    import IPython; IPython.embed()
+    df.to_csv('statistics.csv')
+    raw_result = pd.DataFrame.from_dict(metrics)
+    raw_result.to_csv('raw_result.csv')
+ 
+   
+
+
+
 
 
 def draw_image(V, subVhat, V_noise, sample_index):
@@ -127,3 +164,4 @@ def draw_image(V, subVhat, V_noise, sample_index):
 
 if __name__ == "__main__":
     main()
+
