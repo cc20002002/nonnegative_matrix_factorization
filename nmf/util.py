c51baf0315597ae8b41a174d4eb33c98ea2ccfcf
Joyce Wang <xwan8513@uni.sydney.edu.au>
2018-09-17 13:59:54 +1000

    Adjust figure sizez and lower min error of NMF

--- nmf/util.py
+++ nmf/util.py
@@ -19,8 +19,9 @@ def error_vs_iter(error, niter, algo_name, path):
     """Plot error versus iteration."""
     dataname = path.split("/")[1].split("-")[-1]
     x = min(len(error), niter)
+    pl.figure(figsize=(10, 6))
     pl.plot(np.arange(x), np.log(error))
-    newticks = round(np.exp(pl.yticks()[0]), 2)
+    newticks = np.around(np.exp(pl.yticks()[0]), decimals=2)
     pl.yticks(pl.yticks()[0], newticks)
     pl.xlabel("Iteration")
     pl.ylabel("Error (Log Scale)")
