3c3105ff59b9a5a1460cb53cb27430ea3f823e3f
Joyce Wang <xwan8513@uni.sydney.edu.au>
2018-09-19 20:08:17 +1000

    Resize noise comparison image sizes

--- nmf/vis.py
+++ nmf/vis.py
@@ -3,6 +3,7 @@ import os
 import sys
 import numpy as np
 import pandas as pd
+import matplotlib
 import matplotlib.pyplot as pl
 from nmf import main, io
 
@@ -34,39 +35,39 @@ def draw_error(path1, path2):
 
 def draw_error2():
     """Draw training error of two algorithms together."""
-pl.figure(figsize=(6, 5))
-f1='orl_Multiplication Euclidean_Error_500_Iteration.csv'
-f2='orl_Multiplication KL Divergence_Error_1200_Iteration.csv'
-e1=pd.read_csv(f1)
-e2=pd.read_csv(f2)
-e=[e1,e2*20]    
+    pl.figure(figsize=(6, 5))
+    f1='orl_Multiplication Euclidean_Error_500_Iteration.csv'
+    f2='orl_Multiplication KL Divergence_Error_1200_Iteration.csv'
+    e1=pd.read_csv(f1)
+    e2=pd.read_csv(f2)
+    e=[e1,e2*20]
+    for ii in range(2):
+        error = e[ii]
+        niter = max(int(f1.split("_")[-2]),int(f2.split("_")[-2]))
+        x = min(len(error), niter)
+        #pl.subplot(1, 2, i + 1)
+        pl.loglog(np.arange(x), error)
+        #newticks = np.around(np.exp(pl.yticks()[0]), decimals=2)
+        #pl.yticks(pl.yticks()[0], newticks)
+    pl.figure(figsize=(6, 5))
+    pl.loglog(np.arange(500), e1)
+    pl.loglog(np.arange(x), e2*190)
+    pl.xlabel("Iteration")
+    pl.ylabel("Training Error")
+    pl.title("Error versus Iteration")
 
+    pl.legend(('NMF','KLNMF'))
+    pl.show()
+    pl.savefig("Error.pdf")
+    e11=e1.iloc[100:]
+    e22=e2.iloc[100:]
+    e11.iloc[0]/e11.iloc[15]
+    e22.iloc[0]/e22.iloc[100]
 
-for ii in range(2):
-    error = e[ii]    
-    niter = max(int(f1.split("_")[-2]),int(f2.split("_")[-2]))    
-    x = min(len(error), niter)
-    #pl.subplot(1, 2, i + 1)
-    pl.loglog(np.arange(x), error)
-    #newticks = np.around(np.exp(pl.yticks()[0]), decimals=2)
-    #pl.yticks(pl.yticks()[0], newticks)
-pl.figure(figsize=(6, 5))
-pl.loglog(np.arange(500), e1)
-pl.loglog(np.arange(x), e2*190)
-pl.xlabel("Iteration")
-pl.ylabel("Training Error")
-pl.title("Error versus Iteration")
-
-pl.legend(('NMF','KLNMF'))
-pl.show()
-pl.savefig("Error.pdf")
-e11=e1.iloc[100:]
-e22=e2.iloc[100:]
-e11.iloc[0]/e11.iloc[15]
-e22.iloc[0]/e22.iloc[100]
 
 def draw_noise(dataname):
     """Draw image before and after adding noise."""
+    matplotlib.rcParams.update({'font.size': 6})
     # load data
     assert dataname in ["ORL", "CroppedYaleB"]
     if dataname == "ORL":
@@ -82,10 +83,11 @@ def draw_noise(dataname):
         if noise_name == "No Noise":
             continue
         noise_path = "Noise_{}_{}_Comparison.pdf".format(dataname, noise_name)
+        noise_path = noise_path.replace(" ", "_")
         V, V_noise = main.Noise[noise_name](Vhat)
         img_size = [x // my_scale for x in my_img_size]
         reshape_size = [img_size[1], img_size[0]]
-        pl.figure(figsize=(10,6))
+        pl.figure(figsize=(5, 3))
         pl.subplot(131)
         pl.imshow(Vhat[:, sample_index].reshape(reshape_size), cmap=cmap)
         pl.title('Image (Original)')
@@ -96,9 +98,7 @@ def draw_noise(dataname):
         pl.imshow(V[:, sample_index].reshape(reshape_size), cmap=cmap)
         pl.title('Image (with {} Noise)'.format(noise_name))
         print("Saving to", noise_path)
-        pl.savefig(noise_path+'.pdf', bbox_inches="tight")
-
-
+        pl.savefig(noise_path, bbox_inches="tight")
 
 
 if __name__ == "__main__":
@@ -111,3 +111,5 @@ if __name__ == "__main__":
         draw_error(path1, path2)
     elif argv[1] == "noise":
         draw_noise(argv[2])
+    elif argv[1] == "error2":
+        draw_error2()
