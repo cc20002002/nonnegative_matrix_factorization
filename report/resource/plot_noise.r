4bc034568468c2eff5f5934eeefc6c3c25ce4798
chen chen <chen.chen.adl@gmail.com>
2018-09-21 11:32:40 +1000

    a few plots

--- report/resource/plot_noise.r
+++ report/resource/plot_noise.r
@@ -2,15 +2,18 @@ require('ggplot2')
 library(ggthemes)
 theme_set(theme_bw())  # from ggthemes
 require(data.table)
-x=t(t(c(-20:20)))
+x=t(t(c(-20:20)))*5
+y0=t(t(dnorm(x,mean=0,sd=80)))
 y1=t(t(dnorm(x,mean=0,sd=sqrt(40))))
 y2=t(t(dpois(x+40,lambda = 40)))
-df=cbind(rbind(x,x),rbind(y1,y2))
+df=cbind(rbind(x,x,x),rbind(y0,y1,y2))
 df=data.table(df)
 df=cbind(df,'Gaussian')
-df[42:82,3]='Poisson'
+df[1:41,3]='Gaussian noise N(0,80)'
+df[42:82,3]='Gaussian noise N(0,sqrt(40)) '
+df[83:123,3]='Poisson noise'
 names(df)=c('Noise','Density','Distributions')
 ggplot(df, aes(x=Noise, y=Density,color=Distributions)) + 
-  geom_line() 
+  geom_line(alpha=0.2) 
 
-ggsave('noise.pdf',width = 8, units = "cm")
+ggsave('noise.pdf',width = 7,height = 4, units = "in")
